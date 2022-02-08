27fd92025ce8108ec22c85e659e57dec4aa1362e
Thiago Jose <thiago02_08@hotmail.com>
2022-02-08 11:20:02 -0300

    exemplo de css inline style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 class Button extends React.Component {
     render() {
+        const estaAtivo = true
+        const styles = {
+            backgroundColor: estaAtivo ? "green" : "red"
+        }
         return (
-            <button>
+            <button style={styles}>
                 botão
             </button>
         )
