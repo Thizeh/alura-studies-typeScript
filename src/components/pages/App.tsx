6653cda05eac53b14c0616430b5389695f7afa1a
Thiago Jose <thiago02_08@hotmail.com>
2022-02-08 11:31:25 -0300

    Aplicação do style.scss no App.tsx, organização de pastas

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Form from './components/Form';
-import List from './components/List';
-
+import Form from '../Form';
+import List from '../List';
+import './style.scss'
 
 function App() {
   return (
-    <div className="App">
+    <div className="AppStyle">
       <Form />
       <List />
     </div>
