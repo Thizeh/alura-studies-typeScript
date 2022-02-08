6bc896789939a45ddc103faece4bf8e4bbf90e31
Thiago Jose <thiago02_08@hotmail.com>
2022-02-08 16:51:06 -0300

    adicionando props e children ao botao

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,7 @@ class Button extends React.Component {
     render() {
         return (
             <button className={style.botao}>
-                botão
+                {this.props.children}
             </button>
         )
     }
