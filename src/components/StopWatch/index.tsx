45d2569cc240206e39ef188d13778d801f720713
Thiago Jose <thiago02_08@hotmail.com>
2022-02-22 16:34:24 -0300

    cria o cronometro e o relogio

---
+++ src/components/StopWatch/index.tsx
@@ -0,0 +1,12 @@
+import Button from "../Button";
+
+export function StopWatch() {
+    return (
+        <div>
+            <p>Escolha um card e inicie o cronômetro</p>
+            <Button>
+                Começar
+            </Button>
+        </div>
+    )
+}
