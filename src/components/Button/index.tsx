e4c20608e841bb5cde23e9dabc20ef5d02814b88
Thiago Jose <thiago02_08@hotmail.com>
2022-03-16 16:50:38 -0300

    reafatorando botao e formulario, fim do projeto

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import style from "./Button.module.scss"
 
-class Button extends React.Component<{
-    type?: "button" | "submit" | "reset" | undefined, onClick?: () => void
-}> {
-    render() {
-        const { type = "button", onClick } = this.props;
-        return (
-            <button onClick={onClick} type={type} className={style.botao}>
-                {this.props.children}
-            </button>
-        )
-    }
+interface Props {
+    type?: "button" | "submit" | "reset" | undefined, onClick?: () => void,
+    children?: React.ReactNode
+}
+
+function Button({ onClick, type, children }: Props) {
+    return (
+        <button
+            onClick={onClick}
+            type={type}
+            className={style.botao}
+        >
+            {children}
+        </button>
+    )
 }
 
 export default Button;
